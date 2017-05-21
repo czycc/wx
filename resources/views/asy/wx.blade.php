a454c36c2f1cc3c32b32a2cc4f5fc58bb10dc43d
zhaoyang <602155776@qq.com>
2017-05-21 12:06:44 +0800

    socket.js

--- resources/views/asy/wx.blade.php
+++ resources/views/asy/wx.blade.php
@@ -1,205 +1,286 @@
 <!DOCTYPE html>
 <html>
 
-	<head>
-		<meta charset="utf-8" />
-		<meta name="description" content="安视优">
-		<meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1, maximum-scale=1,,user-scalable=no">
-		<meta name="format-detection" content="telephone=no,email=no" />
-		<meta http-equiv="X-UA-Compatible" content="chrome=1" />
-		<title>安视优</title>
-		<link rel="stylesheet" href="{{asset('asy/css/eyeCommon.css')}}" />
-		<link rel="stylesheet" href="{{asset('asy/css/eye.css')}}" />
-	</head>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="安视优">
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1.0, minimum-scale=1, maximum-scale=1,,user-scalable=no">
+    <meta name="format-detection" content="telephone=no,email=no"/>
+    <meta http-equiv="X-UA-Compatible" content="chrome=1"/>
+    <title>安视优</title>
+    <link rel="stylesheet" href="{{asset('asy/css/eyeCommon.css')}}"/>
+    <link rel="stylesheet" href="{{asset('asy/css/eye.css')}}"/>
+</head>
 
-	<body>
-		<!--第一页-->
-		<div class="page page1 hidden">
-			<div class="top pos">
-				<img src="{{asset('asy/eyeImg/p1/center.png')}}" />
-			</div>
-			<div class="beginPlay ">
-				<img src="{{asset('asy/eyeImg/p1/beginPlayBtm.png')}}" />
-			</div>
-			<div class="intruText pos">
-				<img src="{{asset('asy/eyeImg/p1/instruText.png')}}" />
-			</div>
-			<div class="shake pos">
-				<div class="shakeText">
-					<img src="{{asset('asy/eyeImg/shakeText.png')}}" />
-				</div>
-				<div class="shakeImg">
-					<img src="{{asset('asy/eyeImg/shake.png')}}" />
-				</div>
+<body>
+<!--第一页-->
+<div class="page page1 hidden">
+    <div class="top pos">
+        <img src="{{asset('asy/eyeImg/p1/center.png')}}"/>
+    </div>
+    <div class="beginPlay ">
+        <img src="{{asset('asy/eyeImg/p1/beginPlayBtm.png')}}"/>
+    </div>
+    <div class="intruText pos">
+        <img src="{{asset('asy/eyeImg/p1/instruText.png')}}"/>
+    </div>
+    <div class="shake pos">
+        <div class="shakeText">
+            <img src="{{asset('asy/eyeImg/shakeText.png')}}"/>
+        </div>
+        <div class="shakeImg">
+            <img src="{{asset('asy/eyeImg/shake.png')}}"/>
+        </div>
 
-			</div>
-		</div>
-		<!--第二页-->
-		<div class="page page2 hidden">
-			<div class="day pos">
-				<img src="{{asset('asy/eyeImg/p2/one.png')}}" />
-			</div>
-			<div class="eye pos">
-				<img src="{{asset('asy/eyeImg/p2/eye.png')}}" />
-			</div>
+    </div>
+</div>
+<!--第二页-->
+<div class="page page2 hidden">
+    <div class="day pos">
+        <img src="{{asset('asy/eyeImg/p2/one.png')}}"/>
+    </div>
+    <div class="eye pos">
+        <img src="{{asset('asy/eyeImg/p2/eye.png')}}"/>
+    </div>
 
-			<div class="center pos">
-				<img src="{{asset('asy/eyeImg/eyeball_icon.png')}}" />
-			</div>
-			<div class="shake pos">
-				<div class="shakeText">
-					<img src="{{asset('asy/eyeImg/shakeText.png')}}" />
-				</div>
-				<div class="shakeImg">
-					<img src="{{asset('asy/eyeImg/shake.png')}}" />
-				</div>
+    <div class="center pos">
+        <img src="{{asset('asy/eyeImg/eyeball_icon.png')}}"/>
+    </div>
+    <div class="shake pos">
+        <div class="shakeText">
+            <img src="{{asset('asy/eyeImg/shakeText.png')}}"/>
+        </div>
+        <div class="shakeImg">
+            <img src="{{asset('asy/eyeImg/shake.png')}}"/>
+        </div>
 
-			</div>
-		</div>
-		<!--第三页-->
-		<div class="page page3 ">
-			<div class="day pos">
-				<img src="{{asset('asy/eyeImg/p3/seven.png')}}" />
-			</div>
-			<div class="eye pos">
-				<img src="{{asset('asy/eyeImg/p2/eye.png')}}" />
-			</div>
-			<div class="grem redgrem">
-				<img src="{{asset('asy/eyeImg/germ/grem1.png')}}" />
-			</div>
-			<div class="grem blackgrem">
-				<img src="{{asset('asy/eyeImg/germ/grem7.png')}}" />
-			</div>
-			<div class="grem bluegrem">
-				<img src="{{asset('asy/eyeImg/germ/grem5.png')}}" />
-			</div>
+    </div>
+</div>
+<!--第三页-->
+<div class="page page3 ">
+    <div class="day pos">
+        <img src="{{asset('asy/eyeImg/p3/seven.png')}}"/>
+    </div>
+    <div class="eye pos">
+        <img src="{{asset('asy/eyeImg/p2/eye.png')}}"/>
+    </div>
+    <div class="grem redgrem">
+        <img src="{{asset('asy/eyeImg/germ/grem1.png')}}"/>
+    </div>
+    <div class="grem blackgrem">
+        <img src="{{asset('asy/eyeImg/germ/grem7.png')}}"/>
+    </div>
+    <div class="grem bluegrem">
+        <img src="{{asset('asy/eyeImg/germ/grem5.png')}}"/>
+    </div>
 
-			<div class="center pos">
-				<img src="{{asset('asy/eyeImg/eyeball_icon.png')}}" />
-			</div>
-			{{--<div class="goBtm pos">--}}
-				{{--<img src="{{asset('asy/img/p3/goBtm.png')}}" />--}}
-			{{--</div>--}}
-			<div class="goBtm ">
-				<img src="{{asset('asy/eyeImg/p3/goBtm.png')}}" />
-			</div>
-			<div class="shake pos">
-				<div class="shakeText">
-					<img src="{{asset('asy/eyeImg/shakeText.png')}}" />
-				</div>
-				<div class="shakeImg">
-					<img src="{{asset('asy/eyeImg/shake.png')}}" />
-				</div>
+    <div class="center pos">
+        <img src="{{asset('asy/eyeImg/eyeball_icon.png')}}"/>
+    </div>
+    {{--<div class="goBtm pos">--}}
+    {{--<img src="{{asset('asy/img/p3/goBtm.png')}}" />--}}
+    {{--</div>--}}
+    <div class="goBtm ">
+        <img src="{{asset('asy/eyeImg/p3/goBtm.png')}}"/>
+    </div>
+    <div class="shake pos">
+        <div class="shakeText">
+            <img src="{{asset('asy/eyeImg/shakeText.png')}}"/>
+        </div>
+        <div class="shakeImg">
+            <img src="{{asset('asy/eyeImg/shake.png')}}"/>
+        </div>
 
-			</div>
-		</div>
-		<!--第四页-->
-		<div class="page page4 hidden gremAll">
-			<div class="day pos">
-				<img src="{{asset('asy/eyeImg/p4/ten.png')}}" />
-			</div>
-			<div class="eye pos">
-				<img src="{{asset('asy/eyeImg/p2/eye.png')}}" />
-			</div>
-			<div class="grem redgrem">
-				<img src="{{asset('asy/eyeImg/germ/grem1.png')}}" />
-			</div>
-			<div class="grem blackgrem">
-				<img src="{{asset('asy/eyeImg/germ/grem7.png')}}" />
-			</div>
-			<div class="grem bluegrem">
-				<img src="{{asset('asy/eyeImg/germ/grem5.png')}}" />
-			</div>
-			<div class="grem greengrem">
-				<img src="{{asset('asy/eyeImg/germ/grem2.png')}}" />
-			</div>
-			<div class="grem orangegrem">
-				<img src="{{asset('asy/eyeImg/germ/grem6.png')}}" />
-			</div>
-			<div class="center pos">
-				<img src="{{asset('asy/eyeImg/eyeball_icon.png')}}" />
-			</div>
-			<div class="goBtm ">
-				<img src="{{asset('asy/eyeImg/p3/goBtm.png')}}" />
-			</div>
-			<div class="trowBtm ">
-				<img src="{{asset('asy/eyeImg/p4/throw.png')}}" />
-			</div>
-			<div class="shake pos">
-				<div class="shakeText">
-					<img src="{{asset('asy/eyeImg/shakeText.png')}}" />
-				</div>
-				<div class="shakeImg">
-					<img src="{{asset('asy/eyeImg/shake.png')}}" />
-				</div>
+    </div>
+</div>
+<!--第四页-->
+<div class="page page4 hidden gremAll">
+    <div class="day pos">
+        <img src="{{asset('asy/eyeImg/p4/ten.png')}}"/>
+    </div>
+    <div class="eye pos">
+        <img src="{{asset('asy/eyeImg/p2/eye.png')}}"/>
+    </div>
+    <div class="grem redgrem">
+        <img src="{{asset('asy/eyeImg/germ/grem1.png')}}"/>
+    </div>
+    <div class="grem blackgrem">
+        <img src="{{asset('asy/eyeImg/germ/grem7.png')}}"/>
+    </div>
+    <div class="grem bluegrem">
+        <img src="{{asset('asy/eyeImg/germ/grem5.png')}}"/>
+    </div>
+    <div class="grem greengrem">
+        <img src="{{asset('asy/eyeImg/germ/grem2.png')}}"/>
+    </div>
+    <div class="grem orangegrem">
+        <img src="{{asset('asy/eyeImg/germ/grem6.png')}}"/>
+    </div>
+    <div class="center pos">
+        <img src="{{asset('asy/eyeImg/eyeball_icon.png')}}"/>
+    </div>
+    <div class="goBtm ">
+        <img src="{{asset('asy/eyeImg/p3/goBtm.png')}}"/>
+    </div>
+    <div class="trowBtm ">
+        <img src="{{asset('asy/eyeImg/p4/throw.png')}}"/>
+    </div>
+    <div class="shake pos">
+        <div class="shakeText">
+            <img src="{{asset('asy/eyeImg/shakeText.png')}}"/>
+        </div>
+        <div class="shakeImg">
+            <img src="{{asset('asy/eyeImg/shake.png')}}"/>
+        </div>
 
-			</div>
-		</div>
-		<!--第五页-->
-		<div class="page page5 gremAll hidden">
-			<div class="day pos">
-				<img src="{{asset('asy/eyeImg/p5/fourteen.png')}}" />
-			</div>
-			<div class="eye pos">
-				<img src="{{asset('asy/eyeImg/p2/eye.png')}}" />
-			</div>
-			<div class="grem redgrem">
-				<img src="{{asset('asy/eyeImg/germ/grem1.png')}}" />
-			</div>
-			<div class="grem blackgrem">
-				<img src="{{asset('asy/eyeImg/germ/grem7.png')}}" />
-			</div>
-			<div class="grem bluegrem">
-				<img src="{{asset('asy/eyeImg/germ/grem5.png')}}" />
-			</div>
-			<div class="grem greengrem">
-				<img src="{{asset('asy/eyeImg/germ/grem2.png')}}" />
-			</div>
-			<div class="grem orangegrem">
-				<img src="{{asset('asy/eyeImg/germ/grem6.png')}}" />
-			</div>
-			<div class="grem greensmall">
-				<img src="{{asset('asy/eyeImg/germ/grem3.png')}}" />
-			</div>
-			<div class="grem orangesmall">
-				<img src="{{asset('asy/eyeImg/germ/grem4.png')}}" />
-			</div>
-			<div class="center pos">
-				<img src="{{asset('asy/eyeImg/eyeball_icon.png')}}" />
-			</div>
-			<div class="trowBtm ">
-				<img src="{{asset('asy/eyeImg/p4/throw.png')}}" />
-			</div>
-			<div class="shake pos">
-				<div class="shakeText">
-					<img src="{{asset('asy/eyeImg/shakeText.png')}}" />
-				</div>
-				<div class="shakeImg">
-					<img src="{{asset('asy/eyeImg/shake.png')}}" />
-				</div>
+    </div>
+</div>
+<!--第五页-->
+<div class="page page5 gremAll hidden">
+    <div class="day pos">
+        <img src="{{asset('asy/eyeImg/p5/fourteen.png')}}"/>
+    </div>
+    <div class="eye pos">
+        <img src="{{asset('asy/eyeImg/p2/eye.png')}}"/>
+    </div>
+    <div class="grem redgrem">
+        <img src="{{asset('asy/eyeImg/germ/grem1.png')}}"/>
+    </div>
+    <div class="grem blackgrem">
+        <img src="{{asset('asy/eyeImg/germ/grem7.png')}}"/>
+    </div>
+    <div class="grem bluegrem">
+        <img src="{{asset('asy/eyeImg/germ/grem5.png')}}"/>
+    </div>
+    <div class="grem greengrem">
+        <img src="{{asset('asy/eyeImg/germ/grem2.png')}}"/>
+    </div>
+    <div class="grem orangegrem">
+        <img src="{{asset('asy/eyeImg/germ/grem6.png')}}"/>
+    </div>
+    <div class="grem greensmall">
+        <img src="{{asset('asy/eyeImg/germ/grem3.png')}}"/>
+    </div>
+    <div class="grem orangesmall">
+        <img src="{{asset('asy/eyeImg/germ/grem4.png')}}"/>
+    </div>
+    <div class="center pos">
+        <img src="{{asset('asy/eyeImg/eyeball_icon.png')}}"/>
+    </div>
+    <div class="trowBtm ">
+        <img src="{{asset('asy/eyeImg/p4/throw.png')}}"/>
+    </div>
+    <div class="shake pos">
+        <div class="shakeText">
+            <img src="{{asset('asy/eyeImg/shakeText.png')}}"/>
+        </div>
+        <div class="shakeImg">
+            <img src="{{asset('asy/eyeImg/shake.png')}}"/>
+        </div>
 
-			</div>
+    </div>
 
-		</div>
-		<!--第六页-->
-		<div class="page page6 hidden">
-			<div class="suessTitle pos">
-				<img src="{{asset('asy/eyeImg/p6/wishText.png')}}" />
-			</div>
-			<div class="suessCenter ">
-				<img src="{{asset('asy/eyeImg/p6/eyeball.png')}}" />
-			</div>
-			<div class="suessT pos">
-				<img src="{{asset('asy/eyeImg/p6/suessText.png')}}" />
-			</div>
+</div>
+<!--第六页-->
+<div class="page page6 hidden">
+    <div class="suessTitle pos">
+        <img src="{{asset('asy/eyeImg/p6/wishText.png')}}"/>
+    </div>
+    <div class="suessCenter ">
+        <img src="{{asset('asy/eyeImg/p6/eyeball.png')}}"/>
+    </div>
+    <div class="suessT pos">
+        <img src="{{asset('asy/eyeImg/p6/suessText.png')}}"/>
+    </div>
 
-		</div>
+</div>
 
-	</body>
-	<script src="//{{ Request::getHost() }}:3000/socket.io/socket.io.js"></script>
-	<script src="{{asset('asy/js/jquery-1.11.3..min.js')}}" type="text/javascript" charset="utf-8"></script>
-	<script src="{{asset('asy/js/eye.js')}}" type="text/javascript" charset="utf-8"></script>
+</body>
+<script src="//{{ Request::getHost() }}:3000/socket.io/socket.io.js"></script>
+<script src="{{asset('asy/js/jquery-1.11.3..min.js')}}" type="text/javascript" charset="utf-8"></script>
+<script src="{{asset('asy/js/eye.js')}}" type="text/javascript" charset="utf-8"></script>
+<script type="application/javascript">
+    //开始游戏
+    $('.page1 .beginPlay img').click(function () {
 
+        $('.page2').show().siblings().hide();
+
+    })
+    //第十天继续清洗,扔掉
+
+    //继续清洗
+    $('.page3 .goBtm img').click(function () {
+
+        $('.page4').show().siblings().hide();
+
+    })
+    $('.page4 .goBtm img').click(function () {
+
+        $('.page5').show().siblings().hide();
+
+    })
+
+    //扔掉镜片
+
+    $('.page .trowBtm img').click(function () {
+
+        $('.page6').show().siblings().hide();
+    })
+
+    //摇一摇
+    var SHAKE_THRESHOLD = 1800;
+    var last_update = 0;
+    var x = y = z = last_x = last_y = last_z = 0;
+
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', deviceMotionHandler, false);
+    } else {
+        alert('抱歉，你的手机配置实在有些过不去，考虑换个新的再来试试吧');
+    }
+    var times = 0;
+    var num = 0; //统计摇一摇的次数
+    var socket = io('http://{{ Request::getHost() }}:3000');
+    function deviceMotionHandler(eventData) {
+
+        var acceleration = eventData.accelerationIncludingGravity;
+        var curTime = new Date().getTime();
+
+        if ((curTime - last_update) > 100) {
+            var diffTime = curTime - last_update;
+            last_update = curTime;
+            x = acceleration.x;
+            y = acceleration.y;
+            z = acceleration.z;
+            var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+            var status = document.getElementById("status");
+
+            if (speed > SHAKE_THRESHOLD) {
+
+                doResult();
+
+                num++; //每摇一次，num加1
+                socket.emit('change', num);
+
+            }
+            last_x = x;
+            last_y = y;
+            last_z = z;
+        }
+    }
+
+    function doResult() {
+        if (times > 0) {
+            return false;
+        }
+        $('.shakeImg').addClass('hand');
+
+        setTimeout(function () {
+            times = 0;
+            $('.shakeImg').removeClass('hand');
+        }, 3000);
+    }
+</script>
 
 
 </html>
