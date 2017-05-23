3fe1078b38d62aced3ea6d0fe8ea4a584a663c6c
zhaoyang <602155776@qq.com>
2017-05-23 13:25:16 +0800

    jomoo项目

--- app/Http/Controllers/Wechat/MaterialController.php
+++ app/Http/Controllers/Wechat/MaterialController.php
@@ -21,7 +21,7 @@ class MaterialController extends Controller
 
     public function index()
     {
-        $lists = $material->lists('news', 0, 20);
+        $lists = $this->material->lists('news', 0, 20);
         return $lists;
     }
 }
