935a647e969f7a75171d7e6a6a9cbd36965ee20c
zhaoyang <602155776@qq.com>
2017-05-25 18:21:43 +0800

    匡威项目分享和图片上传接口

--- resources/views/converse.blade.php
+++ resources/views/converse.blade.php
@@ -199,7 +199,7 @@
 <script type="text/javascript" src="{{asset('converse/js/html2canvas.js')}}"></script>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
 <script>
-    wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage'), true) ?>);
+    wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage'), false) ?>);
     // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在 页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready 函数中。
     wx.ready(function(){
         // 获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
