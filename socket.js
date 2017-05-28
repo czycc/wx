f514681f04c4bb643781339f38439a43b23df715
zhaoyang <602155776@qq.com>
2017-05-28 17:04:13 +0800

    安视优项目

--- socket.js
+++ socket.js
@@ -11,6 +11,7 @@ socket.on('connection', function (socket) {
         socket.broadcast.emit('g', {gt: +val});
     });
     socket.on('num', function (val) {
+        console.log('msg:' + val);
         socket.broadcast.emit('s', {shark: +val});
     })
 });
