996a5d9dc4faf60974b047308086f176215492b7
zhaoyang <602155776@qq.com>
2017-05-28 14:05:39 +0800

    匡威项目正式版分享链接图片

--- app/Http/Controllers/Converse/ConverseController.php
+++ app/Http/Controllers/Converse/ConverseController.php
@@ -93,7 +93,7 @@ class ConverseController extends Controller
         //用户头像 base64编码
         //背景图片
         $img = Image::make(public_path('converse/img/hot.png'));
-        $avatar = Image::make($request->input('avatar'))->resize(68, 98);
+        $avatar = Image::make($request->input('avatar'))->resize(74, 98);
         $mask = Image::make(public_path('converse/img/mask2.png'));
         $avatar = $avatar->mask($mask);
         //根据输入字段长度确定白底长度
