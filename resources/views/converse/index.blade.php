8408615029a80acdd40a64395c3e0830bc2cdd7a
zhougogo <1290612478@qq.com>
2017-05-31 16:39:23 +0800

    匡威

--- resources/views/converse/index.blade.php
+++ resources/views/converse/index.blade.php
@@ -8,19 +8,49 @@
  	<div class="page page1 pageBegin">
 		<img src="{{asset('converse/img/loading/loading2-fix.gif')}}">
 	</div>
+	<div class="page page2 hidden" id="page2">
+		<div class="top">
+
+		</div>
+
+		<div class="bottom">
+			<a href="{{ url('kw/cool') }}" class="lBtm btm"><img src="{{asset('converse/img/mode/lBtm.png')}}" /></a>
+			<a href="{{ url('kw/hot') }}" class="sayBtm btm"><img src="{{asset('converse/img/mode/sayBtm.png')}}" /></a>
+		</div>
+		<div class="popup">
+
+		</div>
+
+	</div>
 
 @endsection
 
 @section('javascript')
 <script type="application/javascript">
-    $(function () {
-        // 第一页跳转
-        setTimeout(function() {
-            window.location.href = 'http://wx.touchworld-sh.com/kw/select';
-            // setTimeout(function() {
-            // 	$('.page2').animate({ 'top': '-200%' }, 3000);
-            // }, 1500)
-        }, 2500);
-    })
+//  $(function () {
+//      // 第一页跳转
+//      setTimeout(function() {
+//          window.location.href = 'http://wx.touchworld-sh.com/kw/select';
+//          // setTimeout(function() {
+//          // 	$('.page2').animate({ 'top': '-200%' }, 3000);
+//          // }, 1500)
+//      }, 2500);
+//  })
+
+		var page2 = document.getElementById('page2');
+
+		page2.addEventListener('touchmove', function(e) {
+
+			e.preventDefault();
+
+		});
+		
+		setTimeout(function() {
+			$(".page1").hide();
+			$(".page2").show();
+			setTimeout(function() {
+				$('.page2').animate({ 'top': '-200%' }, 3000);
+			}, 1500)
+		}, 2500);
 </script>
 @endsection
