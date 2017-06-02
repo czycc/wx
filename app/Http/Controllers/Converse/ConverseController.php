5123d19bd7265ea5c9cd4a8551c8afb4c301b38f
zhaoyang <602155776@qq.com>
2017-06-02 18:04:43 +0800

    数梦工厂二维码图片地址

--- app/Http/Controllers/Converse/ConverseController.php
+++ app/Http/Controllers/Converse/ConverseController.php
@@ -125,8 +125,10 @@ class ConverseController extends Controller
         $img_path = Storage::disk('public')->putFile('converse/upload', new File(public_path('converse/upload/1.jpg')));
         $img_url = env('APP_URL') . '/' . $img_path;
 
-        $js = $this->js;
-        return view('converse.poster', compact('img_url', 'js'));
+//        $js = $this->js;
+//        return view('converse.poster', compact('img_url', 'js'));
+          return view('converse.poster', compact('img_url'));
+
 //        return $img->response('png');
     }
 
@@ -225,8 +227,10 @@ class ConverseController extends Controller
         $img_path = Storage::disk('public')->putFile('converse/upload', new File(public_path('converse/upload/1.jpg')));
         $img_url = env('APP_URL') . '/' . $img_path;
 
-        $js = $this->js;
-        return view('converse.poster', compact('img_url', 'js'));
+//        $js = $this->js;
+//        return view('converse.poster', compact('img_url', 'js'));
+        return view('converse.poster', compact('img_url'));
+
 //        return $img->response('png');
 
     }
