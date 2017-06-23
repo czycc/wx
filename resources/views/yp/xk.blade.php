dba75cb55face1cc4917746c34ee4a0cfa4ded3c
zhaoyang <602155776@qq.com>
2017-06-23 12:46:21 +0800

    一品皇家 验证码接口

--- resources/views/yp/xk.blade.php
+++ resources/views/yp/xk.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <title></title>
     <meta name="viewport" content="width=640,target-densitydpi=device-dpi,user-scalable=no">
     <link rel="stylesheet" type="text/css" href="{{ asset('yp/css/index.css') }}"/>
@@ -9,13 +9,15 @@
 <body>
 <!--不符合注册礼领取条件-->
 <div class="all activity">
-    <img src="{{ asset('yp/img/acctivity/activitybg.png') }}" alt="" class="activitybg" />
+    <img src="{{ asset('yp/img/acctivity/activitybg.png') }}" alt="" class="activitybg"/>
     <img src="{{ asset('yp/img/acctivity/acctibtn.png') }}" class="activitybtn" onclick="change()"/>
 </div>
 <!--未购买产品用户页面-->
 <div class="undraw all hidden">
     <img class="undrawbg" src="{{ asset('yp/img/wlq.png') }}"/>
-    <img src="{{ asset('yp/img/undrawbtn.png') }}" alt="" class="undrawbtn" />
+    <a href="http://frisowechat.rfc-china.com/Friso/GiftYPHJ/index.htm">
+        <img src="{{ asset('yp/img/undrawbtn.png') }}" alt="" class="undrawbtn"/>
+    </a>
 </div>
 </body>
 
