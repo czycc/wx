f69fd2e3af07963c729b1a7b213b1d9e00781872
zhaoyang <602155776@qq.com>
2017-06-26 19:26:55 +0800

    一品皇家 测试

--- app/Http/Controllers/YpController.php
+++ app/Http/Controllers/YpController.php
@@ -183,7 +183,7 @@ class YpController extends Controller
                 $prize_code = 'YPHJ04';
                 break;
         }
-        $url = 'http://tt.wedochina.cn/API/CampaignGiftWechatService.asmx?op=GetCommonQrcode';
+        $url = 'http://tt.wedochina.cn/Test/API/CampaignGiftWechatService.asmx?op=GetCommonQrcode';
         $post_data ='<?xml version="1.0" encoding="utf-8"?>
 <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
   <soap12:Header>
